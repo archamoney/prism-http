41654d90ddaa57380c22cfd0f9056a2726921871
Morgan Pretty <morgan.pretty@archa.com.au>
2020-05-06 15:29:50 +1000

    Added x-exclude and fixed a bug with x-static.

--- dist/mocker/generator/JSONSchema.js
+++ dist/mocker/generator/JSONSchema.js
@@ -18,14 +18,47 @@ jsf.option({
 });
 function generate(source) {
     return Either_1.tryCatch(() => {
-        const dynamicResult = jsf.generate(lodash_1.cloneDeep(source));
-        if (source && source.properties && dynamicResult) {
+        const processedSource = lodash_1.cloneDeep(source)
+        if (processedSource.oneOf) { processedSource.oneOf = processedSource.oneOf.filter(o => !o['x-exclude']); }
+        if (processedSource.anyOf) { processedSource.anyOf = processedSource.anyOf.filter(o => !o['x-exclude']); }
+        if (processedSource.allOf) { processedSource.allOf = processedSource.allOf.filter(o => !o['x-exclude']); }
+        
+        const dynamicResult = jsf.generate(lodash_1.cloneDeep(processedSource));
+        const sourceDataSet = (processedSource.oneOf ? processedSource.oneOf : (processedSource.allOf ? processedSource.allOf : processedSource.anyOf));
+        const targetSource = (processedSource.properties ?
+            processedSource :
+            (!sourceDataSet ?
+                processedSource :
+                lodash_1.find(sourceDataSet, o => {
+                    if (!o.properties) { return dynamicResult; }
+
+                    const oKeys = Object.keys(o.properties);
+                    const resultKeys = Object.keys(dynamicResult);
+                    const largerKeys = (oKeys.length > resultKeys.length ? oKeys : resultKeys);
+                    const smallerKeys = (oKeys.length < resultKeys.length ? oKeys : resultKeys);
+
+                    return (largerKeys.length == smallerKeys.length && lodash_1.some(largerKeys, key => smallerKeys.includes(key)));
+                })
+            )
+        );
+
+        if (targetSource && targetSource.properties && dynamicResult) {
             for (const key in dynamicResult) {
-                if (source.properties[key] && source.properties[key]['x-static']) {
-                    dynamicResult[key] = source.properties[key]['x-static'];
+                if (targetSource.properties[key]) {
+                    if (targetSource.properties[key]['x-static']) {
+                        dynamicResult[key] = targetSource.properties[key]['x-static'];
+                    }
+                    else if (targetSource.properties[key].allOf) {
+                        const valueContainingObject = lodash_1.find(targetSource.properties[key].allOf, o => o['x-static']);
+
+                        if (valueContainingObject) {
+                            dynamicResult[key] = valueContainingObject['x-static'];
+                        }
+                    }
                 }
             }
         }
+
         return dynamicResult;
     }, Either_1.toError);
 }
--- src/mocker/generator/JSONSchema.ts
+++ src/mocker/generator/JSONSchema.ts
@@ -23,14 +23,45 @@ jsf.option({
 
 export function generate(source: JSONSchema): Either<Error, unknown> {
   return tryCatch(() => {
-    const dynamicResult = jsf.generate(cloneDeep(source));
+    const processedSource = cloneDeep(source);
+    if (processedSource.oneOf) { processedSource.oneOf = processedSource.oneOf.filter(o => !o['x-exclude']); }
+    if (processedSource.anyOf) { processedSource.anyOf = processedSource.anyOf.filter(o => !o['x-exclude']); }
+    if (processedSource.allOf) { processedSource.allOf = processedSource.allOf.filter(o => !o['x-exclude']); }
+    
+    const dynamicResult = jsf.generate(cloneDeep(processedSource));
+    const sourceDataSet = (processedSource.oneOf ? processedSource.oneOf : (processedSource.allOf ? processedSource.allOf : processedSource.anyOf));
+    const targetSource = (processedSource.properties ?
+      processedSource :
+      (!sourceDataSet ?
+        processedSource :
+        find(sourceDataSet, o => {
+          if (!o.properties) { return dynamicResult; }
 
-    if (source && source.properties && dynamicResult) {
-        for (const key in dynamicResult) {
-            if (source.properties[key] && source.properties[key]['x-static']) {
-                dynamicResult[key] = source.properties[key]['x-static'];
+          const oKeys = Object.keys(o.properties);
+          const resultKeys = Object.keys(dynamicResult);
+          const largerKeys = (oKeys.length > resultKeys.length ? oKeys : resultKeys);
+          const smallerKeys = (oKeys.length < resultKeys.length ? oKeys : resultKeys);
+
+          return (largerKeys.length == smallerKeys.length && some(largerKeys, key => smallerKeys.includes(key)));
+        })
+      )
+    );
+
+    if (targetSource && targetSource.properties && dynamicResult) {
+      for (const key in dynamicResult) {
+        if (targetSource.properties[key]) {
+          if (targetSource.properties[key]['x-static']) {
+            dynamicResult[key] = targetSource.properties[key]['x-static'];
+          }
+          else if (targetSource.properties[key].allOf) {
+            const valueContainingObject = lodash_1.find(targetSource.properties[key].allOf, o => o['x-static']);
+
+            if (valueContainingObject) {
+              dynamicResult[key] = valueContainingObject['x-static'];
             }
+          }
         }
+      }
     }
     
     return dynamicResult;
